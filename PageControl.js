ca3a07a09b096af2ed32f98fad3ddddedc76b063
Max Porter <mx.porters@gmail.com>
2024-01-09 16:43:34 -0800

    updated itd page and general styles and added swipe page switch animation

--- PageControl.js
+++ PageControl.js
@@ -3,56 +3,60 @@ import activeCardContainer from "./ActiveCardContainer.js";
 const dropoffLocationElement = document.querySelector(".dropoff-location-card");
 const overlayCoverElement = document.querySelector(".overlay-cover");
 const dragCardInfoElement = document.querySelector(".drag-card-info");
+const startPageCover = document.querySelector(".start-page-cover");
 
 const cardPageSections = document.querySelectorAll(".card-page-section");
 
 // listen for updated card event
 document.addEventListener("updatedCard", (e) => {
-  const activeCard = activeCardContainer.GetActiveCard();
+    const activeCard = activeCardContainer.GetActiveCard();
 
-  // change url to match card
-  const cardId = activeCard.GetId();
-  window.history.pushState({}, "", `?card=${cardId}`);
+    // change url to match card
+    const cardId = activeCard.GetId();
+    window.history.pushState({}, "", `?card=${cardId}`);
 
-  console.log(activeCard);
-  dragCardInfoElement.classList.add("drag-card-info--hidden");
+    console.log(activeCard);
+    dragCardInfoElement.classList.add("drag-card-info--hidden");
 
-  MoveCardContainer();
-  ActivateOverlay();
-  setTimeout(() => {
-    ScrollToTop();
-    RemoveOverlay();
-    HideAllPages();
-    ShowPage(activeCard.GetId());
-  }, 500);
+    MoveCardContainer();
+    ActivateOverlay();
+    setTimeout(() => {
+        ScrollToTop();
+        HideAllPages();
+        ShowPage(activeCard.GetId());
+        startPageCover.classList.add("hidden");
+    }, 500);
+    setTimeout(() => {
+        RemoveOverlay();
+    }, 1000);
 });
 
 function MoveCardContainer() {
-  dropoffLocationElement.classList.add("dropoff-location-card--moved");
+    dropoffLocationElement.classList.add("dropoff-location-card--moved");
 
-  setTimeout(() => {
-    dropoffLocationElement.classList.add("dropoff-location-card--moved-offscreen");
-  }, 600);
+    setTimeout(() => {
+        dropoffLocationElement.classList.add("dropoff-location-card--moved-offscreen");
+    }, 600);
 }
 
 function ActivateOverlay() {
-  overlayCoverElement.classList.remove("overlay-cover--hidden");
+    overlayCoverElement.classList.add("overlay-cover--active");
 }
 function RemoveOverlay() {
-  overlayCoverElement.classList.add("overlay-cover--hidden");
+    overlayCoverElement.classList.remove("overlay-cover--active");
 }
 
 function HideAllPages() {
-  cardPageSections.forEach((section) => {
-    section.classList.add("card-page-section--hidden");
-  });
+    cardPageSections.forEach((section) => {
+        section.classList.add("card-page-section--hidden");
+    });
 }
 
 function ShowPage(pageId) {
-  const page = document.querySelector(`.card-page-section[data-id="${pageId}"]`);
-  page?.classList.remove("card-page-section--hidden");
+    const page = document.querySelector(`.card-page-section[data-id="${pageId}"]`);
+    page?.classList.remove("card-page-section--hidden");
 }
 
 function ScrollToTop() {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 }
